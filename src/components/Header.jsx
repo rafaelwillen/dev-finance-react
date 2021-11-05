9bf7b494395a8eb20db06f22f8178ddbe9e66864
Rafael Willen <rafaelpadre20@gmail.com>
2021-11-05 13:46:02 +0100

    Add the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="dev.finance$" />
+    </header>
+  );
+};
+
+export default Header;
